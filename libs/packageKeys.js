96905a44fe6f020fc5f077f74c32cc48f65cccc8
Tyrel Chambers <tychambers3@gmail.com>
2021-06-13 21:08:41 -0400

    cp

---
+++ libs/packageKeys.js
@@ -0,0 +1,109 @@
+const keys = [
+  {
+    key: "Name",
+  },
+  {
+    key: "Version",
+  },
+  {
+    key: "Description",
+  },
+  {
+    key: "Keywords",
+  },
+  {
+    key: "Homepage",
+  },
+  {
+    key: "Bugs",
+  },
+  {
+    key: "License",
+  },
+  {
+    key: "Author",
+  },
+  {
+    key: "Contributors",
+  },
+  {
+    key: "Funding",
+  },
+  {
+    key: "Files",
+  },
+  {
+    key: "Main",
+  },
+  {
+    key: "Browser",
+  },
+  {
+    key: "Bin",
+  },
+  {
+    key: "Man",
+  },
+  {
+    key: "Directories - bin",
+  },
+  {
+    key: "Description - man",
+  },
+  {
+    key: "Repository",
+  },
+  {
+    key: "Scripts",
+  },
+  {
+    key: "Config",
+  },
+  {
+    key: "URL dependency",
+  },
+  {
+    key: "Git URL dependency",
+  },
+  {
+    key: "Github URLs",
+  },
+  {
+    key: "Local Paths",
+  },
+  {
+    key: "Dev Dependencies",
+  },
+  {
+    key: "Peer Dependencies",
+  },
+  {
+    key: "Peer Dependencies Meta",
+  },
+  {
+    key: "Bundled Dependencies",
+  },
+  {
+    key: "Optional Dependencies",
+  },
+  {
+    key: "Engines",
+  },
+  {
+    key: "OS",
+  },
+  {
+    key: "CPU",
+  },
+  {
+    key: "Private",
+  },
+  {
+    key: "Publish Config",
+  },
+  {
+    key: "Workspaces",
+  },
+];
+
+keys.map;
